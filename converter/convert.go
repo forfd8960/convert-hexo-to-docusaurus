df061c53259a8ae6bfb5154342cf0607168cf3e0
forfd8960 <forfd8960@gmail.com>
2022-11-24 09:59:27 +0800

    Extract Hexo blog

--- converter/convert.go
+++ converter/convert.go
@@ -1,5 +1,15 @@
 package converter
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	errInvalidBlogFormat = fmt.Errorf("invalid blog format")
+)
+
 /*
 Docusaurus Blog Format
 
@@ -41,18 +51,119 @@ tags: AWS, S3, Hexo
 
 */
 type HexoBlog struct {
-	Title   string   // slug title
-	Date    string   // 2022-06-28
-	Tags    []string //
-	Content string   // blog content
+	SlugTitle string
+	Title     string
+	Date      string   // 2022-06-28
+	Tags      []string //
+	Content   string   // blog content
 }
 
-func ReadHexoBlogs() []*HexoBlog {
-	return nil
+func ReadHexoBlogs(dir string) ([]*HexoBlog, Imgs []string, error) {
+	fs, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var blogFiles []string
+	var blogDirs []string
+	for _, f := range fs {
+		if f.IsDir() {
+			blogDirs = append(blogDirs, f.Name())
+			continue
+		}
+
+		blogFiles = append(blogFiles, f.Name())
+	}
+
+	fmt.Printf("blogDirs: %v\n", blogDirs)
+	fmt.Printf("blogFiles: %v\n", blogFiles)
+
+	var hexoBlogs []*HexoBlog
+	var errs []string
+	for _, name := range blogFiles {
+		blog, err := extractHexoBlog(name, dir+"/"+name)
+		if err != nil {
+			fmt.Printf("extractHexoBlog err: %v, name: %s", err, name)
+			errs = append(errs, err.Error())
+			continue
+		}
+		hexoBlogs = append(hexoBlogs, blog)
+	}
+
+	//todo: collect all imgs
+	return hexoBlogs, nil, nil
 }
 
-func extractContent(blog string) string {
-	return ""
+func extractHexoBlog(name, blogPath string) (*HexoBlog, error) {
+	fileContentBs, err := os.ReadFile(blogPath)
+	if err != nil {
+		return nil, err
+	}
+
+	fileContent := string(fileContentBs)
+	blog, err := extractHeaderAndContent(fileContent)
+	if err != nil {
+		return nil, err
+	}
+	blog.SlugTitle = name
+	return blog, nil
+}
+
+func extractHeaderAndContent(fileContent string) (*HexoBlog, error) {
+	sep := "---"
+	headerIdx := strings.Index(fileContent, sep)
+	if headerIdx == -1 {
+		return nil, errInvalidBlogFormat
+	}
+	lastHeaderIdx := strings.LastIndex(fileContent, sep)
+	if lastHeaderIdx == -1 {
+		return nil, errInvalidBlogFormat
+	}
+
+	start := headerIdx + len(sep)
+	header := fileContent[start:lastHeaderIdx]
+	blog := parseHeader(header)
+
+	start = lastHeaderIdx + len(sep) + len("\n")
+	blog.Content = fileContent[start:]
+	return blog, nil
+}
+
+func parseHeader(header string) *HexoBlog {
+	hds := strings.Split(header, "\n")
+	var title, date string
+	var tags []string
+	/*
+		---
+		title: host-static-blog-on-aws-s3
+		date: 2022-06-28 09:16:48
+		tags: AWS, S3, Hexo
+		---
+	*/
+	for _, hd := range hds {
+		hd = strings.TrimSpace(hd)
+
+		if strings.HasPrefix(hd, "title") {
+			title = strings.Split(hd, ":")[1]
+		}
+		if strings.HasPrefix(hd, "date") {
+			title = strings.Split(hd, ":")[1]
+		}
+		if strings.HasPrefix(hd, "tags") {
+			tagsStr := strings.Split(hd, ":")[1]
+			tags = strings.Split(tagsStr, ", ")
+		}
+	}
+
+	return &HexoBlog{
+		Title: title,
+		Date:  date,
+		Tags:  tags,
+	}
+}
+
+func collectImgsFromBlogDir(blogDirs []string) ([]string, error) {
+	return nil, nil
 }
 
 func replaceImg(concent string) string {
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("load current dir err: ", err)
+		return
+	}
+
+	blogDir := flag.String("", cwd, "hexo blog directory")
+	if len(*blogDir) == 0 {
+		fmt.Fprintln(os.Stderr, "blog dir is empty")
+		return
+	}
+
+	fmt.Println("blog directoty: ", *blogDir)
 }
